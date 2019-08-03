a8845c77c678503dcb838926e03cbadf2da09f6d
antoine <antoine.robert57@icloud.com>
2019-08-03 18:58:39 +0200

    fix dockerhub config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   docker --version
   docker build -t rainbowloutre/mep-front .
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-  docker push rainbowloutre/mep-front;
+  docker push rainbowloutre/dice-roller-web;
 else
   echo "Nothing to build as this is not the master branch"
 fi
