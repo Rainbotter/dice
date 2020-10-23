d38c775e9942a8c033a8b77382b5eb060cf86b4e
Antoine Robert <antoine.robert@protonmail.com>
2020-10-23 10:50:34 +0200

    add e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,46 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    navigateTo(): Promise<any> {
+        return browser.get(browser.baseUrl) as Promise<any>;
+    }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+    getTitleText(): Promise<string> {
+        return element(by.css('app-root h1')).getText() as Promise<string>;
+    }
+
+    roll() {
+        return element(by.id('dice-roll-button')).click() as Promise<void>;
+    }
+
+    selectDiceType(diceType: number): Promise<void> {
+        return element(by.id('dice-' + diceType + '-button')).isPresent().then(isPresent => {
+            if (isPresent) {
+                return element(by.id('dice-' + diceType + '-button')).click() as Promise<void>;
+            } else {
+                return element(by.id('dice-type-other-button')).click().then(
+                    () => element(by.id('dice-value-input')).clear().then(
+                        () => element(by.id('dice-value-input')).sendKeys(diceType))
+                );
+            }
+        }) as Promise<void>;
+    }
+
+    selectDiceNumber(diceNumber: number): Promise<void> {
+
+        return element(by.id('dice-number-' + diceNumber + '-button')).isPresent().then(isPresent => {
+            if (isPresent) {
+                return element(by.id('dice-number-' + diceNumber + '-button')).click() as Promise<void>;
+            } else {
+                return element(by.id('dice-number-other-button')).click().then(
+                    () => element(by.id('dice-number-input')).clear().then(
+                        () => element(by.id('dice-number-input')).sendKeys(diceNumber))
+                );
+            }
+        }) as Promise<void>;
+    }
+
+    getResult(): Promise<string> {
+        return element(by.id('dice-result-text')).getText() as Promise<string>;
+    }
 }
